b92a157a5b2d7832929fb20e229cc64be76b403e
Alberto <Alberto@HAF-932>
2014-12-13 13:14:12 +0100

    v5

--- OsMeusLugares/src/com/example/osmeuslugares/CoordenadasGPS.java
+++ OsMeusLugares/src/com/example/osmeuslugares/CoordenadasGPS.java
@@ -1,17 +1,40 @@
 package com.example.osmeuslugares;
 
+import android.app.Activity;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
 
 public class CoordenadasGPS {
+	private LocationManager manejador;
+	private Activity activity;
+	Location localizacion;
+	String proveedor;
 
-	
-	public CoordenadasGPS(ListLugaresActivity listLugaresActivity) {
-		// TODO Auto-generated constructor stub
+	/**
+	 * @throws Exception
+	 * 
+	 */
+	public CoordenadasGPS(Activity activity) throws Exception {
+		super();
+		this.activity = activity;
+		// inicializar manejador
+		manejador = (LocationManager) activity.getSystemService(proveedor);
+		if (manejador == null)
+			throw new Exception("GPS no disponible");
+		localizacion = manejador
+				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		Criteria criteria = new Criteria();
+		// ejor proveedor red o gps
+		// proveedor = manejador.getBestProvider(criteria, true);
+		// ultima loxalization conocida que es la actual
+		localizacion = manejador.getLastKnownLocation(proveedor);
 	}
 
+	/**
+	 * @return the localizacion
+	 */
 	public Location getLocalizacion() {
-		// TODO Auto-generated method stub
-		return null;
+		return localizacion;
 	}
-
 }
